44a614749fc3f8867cafe5d77701bb0d60a3d1b5
dosanj <javeddosani2011@gmail.com>
2020-04-22 01:25:31 +0530

    Updates

---
+++ precache-manifest.51c6d69e7eaa36da7c9c4b3c100e5d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf9cbd2a4f02de87744a47c8ea442c2",
+    "url": "/jamiatulama-react/index.html"
+  },
+  {
+    "revision": "d07cc2619597210b62cb",
+    "url": "/jamiatulama-react/static/css/main.02ccf131.chunk.css"
+  },
+  {
+    "revision": "23879ee8efa4dadc96b7",
+    "url": "/jamiatulama-react/static/js/2.df431db1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jamiatulama-react/static/js/2.df431db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07cc2619597210b62cb",
+    "url": "/jamiatulama-react/static/js/main.2fb4d80a.chunk.js"
+  },
+  {
+    "revision": "aed4bbff9fdab8c2824d",
+    "url": "/jamiatulama-react/static/js/runtime-main.24c45fdd.js"
+  },
+  {
+    "revision": "caae9c53289478f74bd1b8c4513bbc13",
+    "url": "/jamiatulama-react/static/media/UrduFont.caae9c53.ttf"
+  }
+]);
