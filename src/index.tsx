da01ce3b4ad272447391cb7ec378178a9ff637bd
dosanj <javeddosani2011@gmail.com>
2020-04-22 03:36:25 +0530

    remove swipe

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import { App } from './App';
 import { Switch, Route, HashRouter } from 'react-router-dom';
-import './scripts/swiped-events';
 
 ReactDOM.render(
   <React.StrictMode>
