ab22eb64849e4843a85e9a16f755dee1f6e5885e
dosanj <javeddosani2011@gmail.com>
2020-05-02 02:28:36 +0530

    home page images

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { IImageSliderProps } from '../models/components.props';
 export function ImageSlider(props: IImageSliderProps) {
   return (
-    <div className="slider">
-      <div className="slides">
-        {props.images.map((image) => {
-          return (
-            <div key={image['english-image-title']}>
-              <img src={image.image} alt={image['english-image-title']} />
-            </div>
-          );
-        })}
-      </div>
+    <div className="image-slider">
+      {props.images.map((image) => {
+        return (
+          <div
+            className="image-slider--images"
+            key={image['english-image-title']}
+          >
+            <img src={image.image} alt={image['english-image-title']} />
+            <h4> {image['english-image-title']}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
