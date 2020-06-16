db5498d0d78bb7df02e34a4a9caa26090890a55d
dosanj <javeddosani2011@gmail.com>
2020-06-16 19:57:33 +0530

    change firebase location

--- src/components/BecomeMember.tsx
+++ src/components/BecomeMember.tsx
@@ -2,6 +2,26 @@ import React, { useEffect, useState } from "react";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
 
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+var firebaseConfig = {
+  apiKey: "AIzaSyAaB-8OlMJ-gc1hFxya2hTXEww8rppZwXw",
+  authDomain: "jamiatulama-solapur.firebaseapp.com",
+  databaseURL: "https://jamiatulama-solapur.firebaseio.com",
+  projectId: "jamiatulama-solapur",
+  storageBucket: "jamiatulama-solapur.appspot.com",
+  messagingSenderId: "351010064384",
+  appId: "1:351010064384:web:d5258cf2806d522645b0e5",
+  measurementId: "G-YP619B0WGR",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 export function BecomeMember() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [displayName, setDisplayName] = useState("");
--- src/index.tsx
+++ src/index.tsx
@@ -3,27 +3,6 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import { App } from "./components/App";
 import { HashRouter } from "react-router-dom";
-import * as firebase from "firebase/app";
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-var firebaseConfig = {
-  apiKey: "AIzaSyAaB-8OlMJ-gc1hFxya2hTXEww8rppZwXw",
-  authDomain: "jamiatulama-solapur.firebaseapp.com",
-  databaseURL: "https://jamiatulama-solapur.firebaseio.com",
-  projectId: "jamiatulama-solapur",
-  storageBucket: "jamiatulama-solapur.appspot.com",
-  messagingSenderId: "351010064384",
-  appId: "1:351010064384:web:d5258cf2806d522645b0e5",
-  measurementId: "G-YP619B0WGR",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 ReactDOM.render(
   <HashRouter>
