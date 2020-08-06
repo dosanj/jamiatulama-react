9321def63b5b1b07b12ab81f7b4db2321f4994f8
dosanj <javeddosani2011@gmail.com>
2020-08-07 04:04:16 +0530

    update website purpose

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,41 +1,39 @@
-import React, { MouseEvent } from 'react';
-import { IHomePageProps } from '../models/components.props';
-import { ImageSlider } from './ImageSlider';
-import { homePageData } from '../data/home-page.data';
-import { Languages } from '../services/utility-service';
+import React, { MouseEvent } from "react";
+import { IHomePageProps } from "../models/components.props";
+import { ImageSlider } from "./ImageSlider";
+import { homePageData } from "../data/home-page.data";
+import { Languages } from "../services/utility-service";
 
 export function HomePage(props: IHomePageProps) {
   const openSideMenu = (event: MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
     props.openSideMenu();
   };
-  const ayatMeaning = () => {
-    let name;
+  const getWebsitePurpose = () => {
+    let purpose;
     if (props.currentLanguage === Languages.URDU) {
-      name = homePageData['urdu-ayat-meaning'];
+      return (
+        <div className="home-page__purpose urduText">
+          {homePageData["urdu-purpose"]}
+        </div>
+      );
     }
     if (props.currentLanguage === Languages.HINDI) {
-      name = homePageData['hindi-ayat-meaning'];
+      return (
+        <div className="home-page__purpose">
+          {homePageData["hindi-purpose"]}
+        </div>
+      );
     }
-    if (!name) {
-      name = homePageData['urdu-ayat-meaning'];
-    }
-    return name;
+    return (
+      <div className="home-page__purpose">
+        {homePageData["english-purpose"]}
+      </div>
+    );
   };
   return (
     <div className="home-page">
-      <div className="home-page__heading">
-        <h1 className="arabicFont">
-          اللَّهُمَّ إِنِّي أَسْأَلُكَ عِلْمًا نَافِعًا، وَرِزْقًا طَيِّبًا،
-          وَعَمَلًا مُتَقَبَّلً
-        </h1>
-        <h1>{ayatMeaning()}</h1>
-      </div>
-      <div className="home-page__purpose">
-        Purpose of this website is to connect Ulama Akram with general
-        population. Browse through the website to find advices for many
-        prevelant issues faced by Muslim ummah, Insha Allah.
-      </div>
+      {getWebsitePurpose()}
       <button
         className="home-page__know-more button button-primary"
         onClick={openSideMenu}
@@ -51,7 +49,7 @@ export function HomePage(props: IHomePageProps) {
         </span>
       </div>
       <div className="home-page__image-gallery-slider">
-        <ImageSlider images={homePageData['gallery-images']} />
+        <ImageSlider images={homePageData["gallery-images"]} />
       </div>
     </div>
   );
--- src/data/home-page.data.ts
+++ src/data/home-page.data.ts
@@ -1,16 +1,16 @@
-import data from '../content/home-page.json';
+import data from "../content/home-page.json";
 export interface IHomePageImage {
   image?: string;
-  'english-image-title'?: string;
+  "english-image-title"?: string;
 }
 export interface IHomePageData {
-  'gallery-images': IHomePageImage[];
-  'english-ayat-meaning': string;
-  'english-know-more-button'?: string;
-  'hindi-ayat-meaning'?: string;
-  'hindi-know-more-button'?: string;
-  'urdu-ayat-meaning'?: string;
-  'urdu-know-more-button'?: string;
+  "gallery-images": IHomePageImage[];
+  "english-purpose": string;
+  "english-know-more-button"?: string;
+  "hindi-purpose"?: string;
+  "hindi-know-more-button"?: string;
+  "urdu-purpose"?: string;
+  "urdu-know-more-button"?: string;
 }
 
 export const homePageData: IHomePageData = data;
