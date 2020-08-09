17ebad91e22b8735b21298b18f358057aba27f01
dosanj <javeddosani2011@gmail.com>
2020-08-09 18:49:39 +0530

    return json

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "Hello, World",
+    body: {
+      message: "Hello, World",
+    },
   });
 };
