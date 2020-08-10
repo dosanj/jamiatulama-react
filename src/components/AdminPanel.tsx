adfcde0a0d0d98f540e59e61af63aacefd735096
dosanj <javeddosani2011@gmail.com>
2020-08-10 23:32:08 +0530

    fix build

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { getFireBaseApp } from "../services/firebase-service";
 import * as firebase from "firebase/app";
 getFireBaseApp();
